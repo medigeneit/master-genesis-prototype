2ada184fefd6ff072c05b861f486b43601f49718
msisaif <msi132043@gmail.com>
2024-04-20 09:25:44 +0600

    AppLayout Created

---
+++ resources/views/components/app-layout.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+
+        <title>{{ $title ?? 'Master Genesis Prototype v1' }}</title>
+
+    </head>
+    <body>
+
+        <div id="app">
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+
+        @yield('scripts')
+
+    </body>
+</html>
+<!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+
--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,57 @@ Route::resource('topics', App\Http\Controllers\TopicController::class);
 Route::resource('faculty-disciplines', App\Http\Controllers\FacultyDisciplineController::class);
 
 Route::resource('sessions', App\Http\Controllers\SessionController::class);
+
+
+Route::resource('branches', App\Http\Controllers\BranchController::class);
+
+Route::resource('rooms', App\Http\Controllers\RoomController::class);
+
+Route::resource('mentors', App\Http\Controllers\MentorController::class);
+
+Route::resource('departments', App\Http\Controllers\DepartmentController::class);
+
+Route::resource('institutes', App\Http\Controllers\InstituteController::class);
+
+Route::resource('courses', App\Http\Controllers\CourseController::class);
+
+Route::resource('modules', App\Http\Controllers\ModuleController::class);
+
+Route::resource('batches', App\Http\Controllers\BatchController::class);
+
+Route::resource('bookings', App\Http\Controllers\BookingController::class);
+
+Route::resource('contents', App\Http\Controllers\ContentController::class);
+
+Route::resource('topics', App\Http\Controllers\TopicController::class);
+
+Route::resource('faculty-disciplines', App\Http\Controllers\FacultyDisciplineController::class);
+
+Route::resource('sessions', App\Http\Controllers\SessionController::class);
+
+
+Route::resource('branches', App\Http\Controllers\BranchController::class);
+
+Route::resource('rooms', App\Http\Controllers\RoomController::class);
+
+Route::resource('mentors', App\Http\Controllers\MentorController::class);
+
+Route::resource('departments', App\Http\Controllers\DepartmentController::class);
+
+Route::resource('institutes', App\Http\Controllers\InstituteController::class);
+
+Route::resource('courses', App\Http\Controllers\CourseController::class);
+
+Route::resource('modules', App\Http\Controllers\ModuleController::class);
+
+Route::resource('batches', App\Http\Controllers\BatchController::class);
+
+Route::resource('bookings', App\Http\Controllers\BookingController::class);
+
+Route::resource('contents', App\Http\Controllers\ContentController::class);
+
+Route::resource('topics', App\Http\Controllers\TopicController::class);
+
+Route::resource('faculty-disciplines', App\Http\Controllers\FacultyDisciplineController::class);
+
+Route::resource('sessions', App\Http\Controllers\SessionController::class);
